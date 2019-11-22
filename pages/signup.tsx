caf928184b40905e9357a99bedabeaee7fe3ad1d
Francois Best <francois@francoisbest.com>
2019-11-22 10:30:21 +0100

    feat: Simulate signup process

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import AuthPage from '../src/client/views/auth/AuthPage'
-import SignupForm from '../src/client/views/auth/SignupForm'
+import SignupForm, { Values } from '../src/client/views/auth/SignupForm'
+import { useRouter } from 'next/dist/client/router'
 
 const SignupPage = () => {
+  const router = useRouter()
+  const onSubmit = async (values: Values) => {
+    await new Promise(r => setTimeout(r, 2000))
+    return await router.push('/login')
+  }
+
   return (
     <AuthPage>
-      <SignupForm onSubmit={console.log} />
+      <SignupForm onSubmit={onSubmit} />
     </AuthPage>
   )
 }
