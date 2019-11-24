f56fd22c5f5de6efc064fd0b57eaeb73e84f01b4
Francois Best <francois@francoisbest.com>
2019-11-24 08:50:37 +0100

    feat: Decode all variants of base64

--- src/client/engine/crypto/primitives/codec.test.ts
+++ src/client/engine/crypto/primitives/codec.test.ts
@@ -2,10 +2,36 @@ import { b64, utf8 } from './codec'
 import nacl from 'tweetnacl'
 
 describe('Codecs', () => {
-  test('Base64', () => {
-    const expected = nacl.randomBytes(42)
-    const received = b64.decode(b64.encode(expected))
-    expect(received).toEqual(expected)
+  describe('Base64', () => {
+    test('codec is reversible', () => {
+      const expected = nacl.randomBytes(42)
+      const received = b64.decode(b64.encode(expected))
+      expect(received).toEqual(expected)
+    })
+    test('encoding is correct', () => {
+      const message = 'Hello, World !'
+      const expected = 'SGVsbG8sIFdvcmxkICE='
+      const received = b64.encode(utf8.encode(message))
+      expect(received).toEqual(expected)
+    })
+    test('encoding is url-safe', () => {
+      const message = '👋🌍'
+      const expected = '8J-Ri_CfjI0='
+      const received = b64.encode(utf8.encode(message))
+      expect(received).toEqual(expected)
+    })
+    test('trailing `=` are optional for decoding', () => {
+      const message = '8J-Ri_CfjI0'
+      const expected = '👋🌍'
+      const received = utf8.decode(b64.decode(message))
+      expect(received).toEqual(expected)
+    })
+    test('decoding base64 (standard dictionnary) is supported', () => {
+      const message = '8J+Ri/CfjI0'
+      const expected = '👋🌍'
+      const received = utf8.decode(b64.decode(message))
+      expect(received).toEqual(expected)
+    })
   })
 
   test('UTF-8', () => {
--- src/client/engine/crypto/primitives/codec.ts
+++ src/client/engine/crypto/primitives/codec.ts
@@ -1,9 +1,12 @@
 import { encodeURLSafe, decodeURLSafe } from '@stablelib/base64'
 import { encode, decode } from '@stablelib/utf8'
 
+const urlSafe = (str: string) => str.replace(/\+/g, '-').replace(/\//g, '_')
+
 export const b64 = {
+  urlSafe,
   encode: encodeURLSafe,
-  decode: decodeURLSafe
+  decode: (base64: string) => decodeURLSafe(urlSafe(base64))
 }
 
 export const utf8 = {
