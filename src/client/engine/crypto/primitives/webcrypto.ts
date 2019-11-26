c368e486091720f8291794af598c4c3454e9d237
Francois Best <francois@francoisbest.com>
2019-11-26 11:11:25 +0100

    fix: Don't import NodeWebCrypto in the browser

--- src/client/engine/crypto/primitives/webcrypto.ts
+++ src/client/engine/crypto/primitives/webcrypto.ts
@@ -1,6 +1,11 @@
-import NodeWebCrypto from 'node-webcrypto-ossl'
+const webcryptoFactory = () => {
+  if (typeof window === 'undefined') {
+    // Server side
+    const NodeWebCrypto = require('node-webcrypto-ossl')
+    return new NodeWebCrypto()
+  } else {
+    return window.crypto
+  }
+}
 
-const webcrypto =
-  typeof window === 'undefined' ? new NodeWebCrypto() : window.crypto
-
-export default webcrypto
+export default webcryptoFactory()
