18ccd4ea8b5eefb1213b4f53ad623d073dac6676
Francois Best <francois@francoisbest.com>
2019-12-02 17:11:18 +0100

    fix: Handle all errors
    Example: missing body

--- src/server/middleware/requireBodyParams.ts
+++ src/server/middleware/requireBodyParams.ts
@@ -20,40 +20,47 @@ const requireBodyParams = <Body>(validator: Validator<Body>) => (
   res: NextApiResponse,
   next: any
 ) => {
-  const errors = Object.keys(validator)
-    .map(key => {
-      if (!validator[key](req.body[key])) {
-        if (req.body[key] === undefined) {
+  try {
+    const errors = Object.keys(validator)
+      .map(key => {
+        if (!validator[key](req.body[key])) {
+          if (req.body[key] === undefined) {
+            return {
+              field: key,
+              error: `Required field '${key}' is missing`
+            }
+          }
+          if (req.body[key] === '') {
+            return {
+              field: key,
+              error: `Required field '${key}' has an invalid value: '' (empty string)`
+            }
+          }
+
           return {
             field: key,
-            error: `Required field '${key}' is missing`
+            error: `Required field '${key}' has an invalid value: ${JSON.stringify(
+              req.body[key]
+            )}`
           }
         }
-        if (req.body[key] === '') {
-          return {
-            field: key,
-            error: `Required field '${key}' has an invalid value: '' (empty string)`
-          }
-        }
+        return null
+      })
+      .filter(error => error !== null)
 
-        return {
-          field: key,
-          error: `Required field '${key}' has an invalid value: ${JSON.stringify(
-            req.body[key]
-          )}`
-        }
-      }
-      return null
-    })
-    .filter(error => error !== null)
-
-  if (errors.length > 0) {
+    if (errors.length > 0) {
+      return res.status(400).json({
+        error: 'Invalid body parameters',
+        details: errors
+      })
+    }
+    return next()
+  } catch (error) {
     return res.status(400).json({
       error: 'Invalid body parameters',
-      details: errors
+      details: error.message
     })
   }
-  return next()
 }
 
 export default requireBodyParams
