5ab50c5bb94c070df3c2cb549fd510feb1e3f2b1
Francois Best <francois@francoisbest.com>
2019-12-02 17:10:39 +0100

    ref: Use better generic names

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,15 +1,15 @@
 import { NextApiResponse } from 'next'
 import { RequestWithBody } from './middleware/requireBodyParams'
 
-export type Request<T = {}, B = any> = RequestWithBody<B> & T
+export type Request<Fields = {}, Body = any> = RequestWithBody<Body> & Fields
 
-export type NextApiHandler<T = {}, B = any> = (
-  req: Request<T, B>,
+export type NextApiHandler<Fields = {}, Body = any> = (
+  req: Request<Fields, Body>,
   res: NextApiResponse
 ) => void | Promise<void>
 
-export type NextApiMiddleware<T = {}, B = any> = (
-  req: Request<T, B>,
+export type NextApiMiddleware<Fields = {}, Body = any> = (
+  req: Request<Fields, Body>,
   res: NextApiResponse,
   next: () => void
 ) => void | Promise<void>
