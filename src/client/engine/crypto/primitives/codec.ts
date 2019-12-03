a28c57f3dba59897a7fa3086196e3a6c0eef32ea
Francois Best <francois@francoisbest.com>
2019-12-03 13:47:35 +0100

    feat: Add encodings from Horcrux

--- src/client/engine/crypto/primitives/codec.ts
+++ src/client/engine/crypto/primitives/codec.ts
@@ -27,3 +27,26 @@ export const hexToBase64url = (input: string) => {
 export const base64ToHex = (base64: string) => {
   return hex.encode(b64.decode(base64))
 }
+
+// --
+
+export type Encoder = (buffer: Uint8Array) => string
+export type Decoder = (string: string) => Uint8Array
+export type Encoding = 'base64' | 'utf8' | 'hex'
+export type Encoders = {
+  [key in Encoding]: Encoder
+}
+export type Decoders = {
+  [key in Encoding]: Decoder
+}
+
+export const encoders: Encoders = {
+  base64: b64.encode,
+  utf8: utf8.decode,
+  hex: hex.encode
+}
+export const decoders: Decoders = {
+  base64: b64.decode,
+  utf8: utf8.encode,
+  hex: hex.decode
+}
