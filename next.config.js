826a3dc1db2a61113a3f10acda4f88105fdb8f0d
Francois Best <francois@francoisbest.com>
2019-12-03 21:58:56 +0100

    feat: Bundle size analyzer

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ require('env-alias').default()
 const path = require('path')
 const webpack = require('webpack')
 const withSourceMaps = require('@zeit/next-source-maps')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 const loadFromEnv = names => {
   const out = {}
@@ -13,35 +16,37 @@ const loadFromEnv = names => {
   return out
 }
 
-module.exports = withSourceMaps({
-  // Will be available both in the client and server
-  env: loadFromEnv(['API_URL', 'APP_URL', 'SENTRY_DSN']),
+module.exports = withBundleAnalyzer(
+  withSourceMaps({
+    // Will be available both in the client and server
+    env: loadFromEnv(['API_URL', 'APP_URL', 'SENTRY_DSN']),
 
-  // Will only be available on the server side
-  serverRuntimeConfig: loadFromEnv([
-    'DATABASE_URI',
-    'JWT_SECRET',
-    'JWT_ISSUER',
-    'CLOAKING_MASTER_KEY',
-    'CLOAKING_KEYCHAIN'
-  ]),
-  webpack: (config, { isServer, buildId }) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
+    // Will only be available on the server side
+    serverRuntimeConfig: loadFromEnv([
+      'DATABASE_URI',
+      'JWT_SECRET',
+      'JWT_ISSUER',
+      'CLOAKING_MASTER_KEY',
+      'CLOAKING_KEYCHAIN'
+    ]),
+    webpack: (config, { isServer, buildId }) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.SENTRY_RELEASE': JSON.stringify(buildId)
+        })
+      )
+
+      config.resolve.alias['~'] = path.resolve(__dirname)
+
+      // if (!isServer) {
+      //   config.resolve.alias['@sentry/node'] = '@sentry/browser'
+      // }
+
+      return config
     }
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.SENTRY_RELEASE': JSON.stringify(buildId)
-      })
-    )
-
-    config.resolve.alias['~'] = path.resolve(__dirname)
-
-    // if (!isServer) {
-    //   config.resolve.alias['@sentry/node'] = '@sentry/browser'
-    // }
-
-    return config
-  }
-})
+  })
+)
