668e196726f28b64fbfc206735f1f07c2e8d8ccb
Francois Best <francois@francoisbest.com>
2019-12-05 13:23:03 +0100

    fix: Follow env aliasing as done in next-config.js

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,17 +1,21 @@
 import getServerSideConfig from 'next/config'
 import dotenv from 'dotenv'
+import envAlias from 'env-alias'
 
 // In a Next.js context, we can use the supplied serverRuntimeConfig object,
 // configured in /next.config.js.
-// However in some cases outside of Next (running code in cron tasks)
-// we may need to access the environment, and we use to dotenv to load local
-// .env scripts (don't use those in production) and fallback to the environment.
+// However in some cases outside of Next (eg: running code in cron tasks)
+// we may need to access the environment. We use to dotenv to load local
+// .env scripts (not used in production) and fallback to the environment.
 export default (() => {
   const nextConfig = getServerSideConfig()
   if (nextConfig) {
     return nextConfig.serverRuntimeConfig
   }
-  dotenv.config()
+  if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+    envAlias()
+  }
 
   return process.env
 })()
