7c8f0e38dd3459bab4c8b777b19f04bcac0042a3
Francois Best <francois@francoisbest.com>
2019-12-09 09:39:06 +0100

    feat: Implement local keychain key expiration date

--- src/client/engine/keyStorage.ts
+++ src/client/engine/keyStorage.ts
@@ -2,11 +2,13 @@ import { CloakKey } from './crypto/cloak'
 import { set, get, del } from 'idb-keyval'
 import { expirationTimes } from '~/src/shared/config'
 
+interface ExpirableKey {
+  key: string
+  expiresAt: Date
+}
+
 interface MemoryKeyStorage {
-  keychainKey: {
-    key: string
-    expiresAt: Date
-  }
+  keychainKey: ExpirableKey
 }
 
 const memoryKeyStorage: MemoryKeyStorage = {
@@ -14,33 +16,28 @@ const memoryKeyStorage: MemoryKeyStorage = {
 }
 
 export const saveKeychainKey = async (key: CloakKey, persist: boolean) => {
-  if (persist) {
-    await set('keys:keychain', key)
-  }
   memoryKeyStorage.keychainKey = {
     key,
     expiresAt: expirationTimes.inSevenDays()
+  }
+  if (persist) {
+    await set('keys:keychain', memoryKeyStorage.keychainKey)
   }
 }
 
 export const loadKeychainKey = async () => {
   if (memoryKeyStorage.keychainKey) {
-    const now = new Date()
-    if (memoryKeyStorage.keychainKey.expiresAt < now) {
-      try {
-        // Key is expired, revoke it
-        await deleteKeychainKey()
-      } catch {}
+    if (handleExpiration(memoryKeyStorage.keychainKey)) {
       return null
     }
     return memoryKeyStorage.keychainKey.key
   }
   try {
-    const key = await get<string>('keys:keychain')
-    if (!key) {
-      throw new Error('No key stored in session storage')
+    const key = await get<ExpirableKey>('keys:keychain')
+    if (handleExpiration(key)) {
+      return null
     }
-    return key
+    return key.key
   } catch {}
   return null
 }
@@ -48,4 +45,17 @@ export const loadKeychainKey = async () => {
 export const deleteKeychainKey = async () => {
   memoryKeyStorage.keychainKey = null
   await del('keys:keychain')
+}
+
+// --
+
+const handleExpiration = async (key: ExpirableKey, now = new Date()) => {
+  if (key.expiresAt < now) {
+    try {
+      // Key is expired, revoke it
+      await deleteKeychainKey()
+    } catch {}
+    return true
+  }
+  return false
 }
