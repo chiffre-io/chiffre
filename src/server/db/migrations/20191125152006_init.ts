a62da4cc49fd41ea276d130cda1ded6104f84d06
Francois Best <francois@francoisbest.com>
2019-12-20 08:24:19 +0000

    feat: Add initial down migration

--- src/server/db/migrations/20191125152006_init.ts
+++ src/server/db/migrations/20191125152006_init.ts
@@ -1,14 +1,44 @@
 import Knex from 'knex'
-import { createInitialUsersAuthSrpTable } from '../models/auth/UsersAuthSRP'
-import { createInitialLoginChallengesSrpTable } from '../models/auth/LoginChallengesSRP'
-import { createInitialSessionsTable } from '../models/auth/Sessions'
-import { createInitialUsersAuthSettingsTable } from '../models/auth/UsersAuthSettings'
-import { createInitialKeychainsTable } from '../models/entities/Keychains'
-import { createInitialVaultsTable } from '../models/entities/Vaults'
-import { createInitialProjectsTable } from '../models/entities/Projects'
-import { createInitialProjectMessageQueueTable } from '../models/entities/ProjectMessageQueue'
-import { createInitialUserVaultEdgesTable } from '../models/entities/UserVaultEdges'
-import { setupUpdatedAtFieldAutoUpdate } from '../utility'
+import {
+  createInitialUsersAuthSrpTable,
+  USERS_AUTH_SRP_TABLE
+} from '../models/auth/UsersAuthSRP'
+import {
+  createInitialLoginChallengesSrpTable,
+  LOGIN_CHALLENGES_SRP_TABLE
+} from '../models/auth/LoginChallengesSRP'
+import {
+  createInitialSessionsTable,
+  SESSIONS_TABLE
+} from '../models/auth/Sessions'
+import {
+  createInitialUsersAuthSettingsTable,
+  USERS_AUTH_SETTINGS_TABLE
+} from '../models/auth/UsersAuthSettings'
+import {
+  createInitialKeychainsTable,
+  KEYCHAINS_TABLE
+} from '../models/entities/Keychains'
+import {
+  createInitialVaultsTable,
+  VAULTS_TABLE
+} from '../models/entities/Vaults'
+import {
+  createInitialProjectsTable,
+  PROJECTS_TABLE
+} from '../models/entities/Projects'
+import {
+  createInitialProjectMessageQueueTable,
+  PROJECT_MESSAGE_QUEUE_TABLE
+} from '../models/entities/ProjectMessageQueue'
+import {
+  createInitialUserVaultEdgesTable,
+  USER_VAULT_EDGES_TABLE
+} from '../models/entities/UserVaultEdges'
+import {
+  setupUpdatedAtFieldAutoUpdate,
+  setupBase64IdGenerator
+} from '../utility'
 
 export async function up(knex: Knex): Promise<any> {
   console.info('Setting up database from scratch...')
@@ -34,5 +64,17 @@ export async function up(knex: Knex): Promise<any> {
 }
 
 export async function down(knex: Knex): Promise<any> {
-  console.error('Not reverting initial migration')
+  if (process.env.NODE_ENV === 'production') {
+    console.error('Refusing to run initial down migration in production')
+  }
+
+  await knex.schema.dropTableIfExists(PROJECT_MESSAGE_QUEUE_TABLE)
+  await knex.schema.dropTableIfExists(PROJECTS_TABLE)
+  await knex.schema.dropTableIfExists(USER_VAULT_EDGES_TABLE)
+  await knex.schema.dropTableIfExists(VAULTS_TABLE)
+  await knex.schema.dropTableIfExists(KEYCHAINS_TABLE)
+  await knex.schema.dropTableIfExists(SESSIONS_TABLE)
+  await knex.schema.dropTableIfExists(LOGIN_CHALLENGES_SRP_TABLE)
+  await knex.schema.dropTableIfExists(USERS_AUTH_SETTINGS_TABLE)
+  await knex.schema.dropTableIfExists(USERS_AUTH_SRP_TABLE)
 }
