715676727984f849a007ff7b44df46d3b6a688af
Francois Best <francois@francoisbest.com>
2019-12-25 19:01:20 +0000

    fix: Disable seeds in production

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,25 @@ const dotenv = require('dotenv')
 // Load environment variables
 dotenv.config()
 
-module.exports = {
+const commonConfig = {
   client: 'pg',
   connection: process.env.DATABASE_URI,
   migrations: {
     directory: './src/server/db/migrations',
     extension: 'ts',
     loadExtensions: ['.ts']
+  }
+}
+
+module.exports = {
+  development: {
+    ...commonConfig,
+    seeds: {
+      directory: './src/server/db/seeds',
+      loadExtensions: ['.ts']
+    }
   },
-  seeds: {
-    directory: './src/server/db/seeds',
-    loadExtensions: ['.ts']
+  production: {
+    ...commonConfig
   }
 }
