a3515724b99a3e980497aa8cc0babe5acb354b9b
Francois Best <francois@francoisbest.com>
2020-01-17 12:03:13 +0100

    feat: Add 2FA tests

--- packages/client/test/e2e/client.e2e.test.ts
+++ packages/client/test/e2e/client.e2e.test.ts
@@ -6,6 +6,8 @@ import { EventTypes } from '@chiffre/api/src/db/models/business/Events'
 
 let ctx: TestContext = undefined
 
+jest.useFakeTimers()
+
 beforeAll(async () => {
   ctx = await setup()
 })
@@ -62,6 +64,27 @@ test('Create projects', async () => {
   expect(ctx.client.getProject(project2.id)).toEqual(project2)
 })
 
+test('Account activity', async () => {
+  const activity = await ctx.client.getAccountActivity()
+  expect(activity.length).toBe(4)
+  // Events are sorted most recent first
+  expect(activity[0].type).toEqual(EventTypes.projectCreated)
+  expect(activity[1].type).toEqual(EventTypes.projectCreated)
+  expect(activity[2].type).toEqual(EventTypes.login)
+  expect(activity[3].type).toEqual(EventTypes.signup)
+})
+
+test('Lock', async () => {
+  expect(ctx.client.lock())
+  expect(ctx.client.isLocked).toBeTrue()
+  expect(ctx.client.identity).toBeNil()
+  expect(ctx.client.projects).toBeEmpty()
+  expect(ctx.client.settings.twoFactor.status).toBeNil()
+
+  // Restore state
+  await ctx.client.login('test.user@example.com', 'password')
+})
+
 // --
 
 let totpSecret: string | undefined = undefined
@@ -84,33 +107,48 @@ test('2FA - Enable', async () => {
   expect(ctx.client.settings.twoFactor.status).toEqual(TwoFactorStatus.verified)
 })
 
+test('2FA - Login with valid 2FA token', async () => {
+  ctx.client.lock()
+  const { requireTwoFactorAuthentication } = await ctx.client.login(
+    'test.user@example.com',
+    'password'
+  )
+  expect(requireTwoFactorAuthentication).toBeTrue()
+  expect(ctx.client.settings.twoFactor.status).toEqual(TwoFactorStatus.enabled)
+  const token = generateTwoFactorToken(totpSecret)
+  const p = ctx.client.verifyTwoFactorToken(token)
+  await expect(p).resolves.toBeNil()
+})
+
+test('2FA - Login with invalid 2FA token', async () => {
+  ctx.client.lock()
+  await ctx.client.login('test.user@example.com', 'password')
+  const token = generateTwoFactorToken(totpSecret)
+  const wrongToken = ((parseInt(token) + 1) % 999999).toString()
+  const p = ctx.client.verifyTwoFactorToken(wrongToken)
+  await expect(p).rejects.toContain('Invalid')
+})
+
+test('2FA - Login with valid 2FA token supplied after grace period', async () => {
+  ctx.client.lock()
+  await ctx.client.login('test.user@example.com', 'password')
+  jest.advanceTimersByTime(60 * 1000) // Wait for 1 minute
+  const token = generateTwoFactorToken(totpSecret)
+  const p = ctx.client.verifyTwoFactorToken(token)
+  await expect(p).rejects.toThrowError('Session expired, please log in again')
+})
+
 test('2FA - Disable', async () => {
+  // Reset login state
+  {
+    ctx.client.lock()
+    await ctx.client.login('test.user@example.com', 'password')
+    const token = generateTwoFactorToken(totpSecret)
+    await ctx.client.verifyTwoFactorToken(token)
+  }
   expect(ctx.client.settings.twoFactor.status).toEqual(TwoFactorStatus.verified)
   const token = generateTwoFactorToken(totpSecret)
   await ctx.client.settings.twoFactor.disable(token)
   expect(ctx.client.settings.twoFactor.status).toEqual(TwoFactorStatus.disabled)
   totpSecret = undefined
-})
-
-test('Account activity', async () => {
-  const activity = await ctx.client.getAccountActivity()
-  expect(activity.length).toBe(9)
-  // Events are sorted most recent first
-  expect(activity[1].type).toEqual(EventTypes.twoFactorStatusChanged)
-  expect(activity[1].type).toEqual(EventTypes.twoFactorStatusChanged)
-  expect(activity[2].type).toEqual(EventTypes.twoFactorStatusChanged)
-  expect(activity[3].type).toEqual(EventTypes.twoFactorStatusChanged)
-  expect(activity[4].type).toEqual(EventTypes.twoFactorStatusChanged)
-  expect(activity[5].type).toEqual(EventTypes.projectCreated)
-  expect(activity[6].type).toEqual(EventTypes.projectCreated)
-  expect(activity[7].type).toEqual(EventTypes.login)
-  expect(activity[8].type).toEqual(EventTypes.signup)
-})
-
-test('Lock', () => {
-  expect(ctx.client.lock())
-  expect(ctx.client.isLocked).toBeTrue()
-  expect(ctx.client.identity).toBeNil()
-  expect(ctx.client.projects).toBeEmpty()
-  expect(ctx.client.settings.twoFactor.status).toBeNil()
 })
