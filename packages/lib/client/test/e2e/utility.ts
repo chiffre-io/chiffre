eed0bf69f6f11370dca571d1a46b5972f15c73ce
Francois Best <francois@francoisbest.com>
2020-01-23 12:49:18 +0100

    feat: Use built API for more realistic tests

--- packages/lib/client/test/e2e/utility.ts
+++ packages/lib/client/test/e2e/utility.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import checkEnv from '@47ng/check-env'
-import { createServer, startServer } from '@chiffre/api/src/server'
-import { App } from '@chiffre/api/src/types'
+import { createServer, startServer } from '@chiffre/api/dist/server'
+import { App } from '@chiffre/api/dist/types'
 import path from 'path'
 import Client from '../../src/index'
 
