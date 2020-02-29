d18cf753b006acf54aaec516da8fbcca7d564fb2
Francois Best <francois@francoisbest.com>
2020-02-29 08:41:40 +0100

    feat: Add feature flags
    chiffre:no-send to skip sending data
    chiffre:debug to log events before sending

--- packages/lib/analytics/emitter/src/index.ts
+++ packages/lib/analytics/emitter/src/index.ts
@@ -7,17 +7,23 @@ import {
 } from '@chiffre/analytics-core'
 import { Config } from './types'
 
-function readConfig() {
+function readConfig(): Config {
   try {
     const config = JSON.parse(
       document.getElementById('chiffre:analytics-config').innerText
     )
+    if (!config.pushURL) {
+      throw new Error('Missing pushURL')
+    }
     return {
       publicKey: parsePublicKey(config.publicKey),
-      pushURL: config.pushURL || 'https://push.chiffre.io'
+      pushURL: config.pushURL
     }
   } catch (error) {
-    console.error('Failed to load Chiffre analytics configuration:', error)
+    console.error(
+      '[Chiffre] Failed to load Chiffre analytics configuration:',
+      error
+    )
     return null
   }
 }
@@ -27,10 +33,27 @@ function sendEvent(event: Event<any, any>, config: Config) {
   const payload = JSON.stringify(event)
   const message = encryptString(payload, config.publicKey)
   const tock = performance.now()
-  const url = `${config.pushURL}?perf=${Math.round(tock - tick)}`
+  const perf = Math.round(tock - tick)
+  const url = `${config.pushURL}?perf=${perf}`
+  if (window.localStorage.getItem('chiffre:debug') === 'true') {
+    console.dir({
+      event,
+      message,
+      perf,
+      url
+    })
+  }
+  if (window.localStorage.getItem('chiffre:no-send') === 'true') {
+    console.info('[Chiffre] Skip sending message (chiffre:no-send is set)', {
+      payload: message,
+      perf
+    })
+    return false
+  }
+
   const sent = navigator.sendBeacon(url, message)
   if (!sent) {
-    console.warn('Analytics message failed to send')
+    console.warn('[Chiffre] Analytics message failed to send')
   }
   return sent
 }
